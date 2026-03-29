99e423398e509d5dba016235ce7fb0dd83d94668
Emanuele Patriarca <>
2026-03-29 20:19:07 +0200

    fix config

--- source/processing_service/app/core/config.py
+++ source/processing_service/app/core/config.py
@@ -3,7 +3,7 @@ from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
     PROJECT_NAME: str = "A Fragile Balance of Power"
     DATABASE_URL: str = ""
-    BROKER_WEBSOCKET_URI: str = "http://broker:8000/api/process_data/ws"
+    BROKER_WEBSOCKET_URI: str = "ws://broker:8000/api/process_data/ws"
     SHUTDOWN_SSE_URI: str = "http://simulator:8080/api/control"
 
     class Config:
