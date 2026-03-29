42effe274d537120b76ba11ac850acc2daf06f77
Emanuele Patriarca <>
2026-03-30 00:32:24 +0200

    event categorization

--- source/processing_service/app/services/vibration_analyzer.py
+++ source/processing_service/app/services/vibration_analyzer.py
@@ -63,7 +63,7 @@ class VibrationAnalyzer:
             response = EventDataResponse(
                 sensor_id=self.sensor_id,
                 timestamp=event_time,
-                category_event="", # TODO: Update this based on your own condition/logic
+                category_event=self.categorize_event(dominant_freq),
                 dominant_frequency=float(dominant_freq)
             )
 
@@ -73,3 +73,11 @@ class VibrationAnalyzer:
             
         except Exception as e:
             logger.error(f"Error computing FFT: {e}")
+
+    def categorize_event(self, dominant_frequency):
+        if dominant_frequency < 0.5:
+            return ""
+        elif dominant_frequency < 5.0:
+            return "conventional_explosion"
+        else:
+            return "nuclear_like_event"
