fix broadcast of considerable event to gateway

source/processing_service/app/services/vibration_analyzer.py
@@ -32,6 +32,9 @@ class VibrationAnalyzer:
             self.values = self.values[self.step_size:]
 
     async def calculate_dominant_frequency(self):
+
+        response = None
+
         try:
             t = np.array(self.timestamps)
             y = np.array(self.values)
@@ -52,7 +55,7 @@ class VibrationAnalyzer:
             magnitudes = np.abs(yf)
             dominant_idx = np.argmax(magnitudes)
             dominant_freq = xf[dominant_idx]
-            
+
             # Send the results obtained via WebSocket to API Gateway
 
             event_time = datetime.fromtimestamp(self.timestamps[-1], tz=timezone.utc).isoformat()
@@ -64,35 +67,51 @@ class VibrationAnalyzer:
                 dominant_frequency=float(dominant_freq)
             )
 
-            # Saving considerable events to db only if the previous event detected is different from the current one
-            # Example: Earthquake begin -> event_detected = "earthquake" != "" -> Event saved
-            #          Earthquake continues -> event_detected = "earthquake" == "earthquake" -> No event saved
-            #          Earthquake ends -> event_detected = "" != "earthquake" -> Event not saved being not a considerable event
-            #
-            # This logic is to prevent the same replica to save event multiple time
-
-            if response.category_event != "":
-
-                if self.event_detected != response.category_event:
-                    self.event_detected = response.category_event
-
-                    if self.event_detected != "":
-                        await save_event(response.sensor_id, response.timestamp, response.category_event, response.dominant_frequency)
-
-                print(response.model_dump_json())
-
-
-            await manager.broadcast(response.model_dump_json())
-            
         except Exception as e:
             logger.error(f"Error computing FFT: {e}")
+
+        # Saving considerable events to db only if the previous event detected is different from the current one
+        # Example: Earthquake begin -> event_detected = "earthquake" != "" -> Event saved
+        #          Earthquake continues -> event_detected = "earthquake" == "earthquake" -> No event saved
+        #          Earthquake ends -> event_detected = "" != "earthquake" -> Event not saved being not a considerable event
+        #
+        # This logic w to prevent the same replica to save event multiple time
+
+        # Broadcast to gateway:
+        # A considerable event is sent with category_event != "" only once, then it is sent with category_event = ""
+        # in this way the frontend can correctly populate:
+        # 1) the event history (considerable events are unique)
+        # 2) the graph of all the events (considerable events are shown untile they last)
+
+        if response is not None:
+            try:
+
+                if response.category_event != "":
+
+                    if self.event_detected != response.category_event:
+                        self.event_detected = response.category_event
+
+                        await save_event(response.sensor_id, response.timestamp, response.category_event, response.dominant_frequency)
+                        await manager.broadcast(response.model_dump_json())
+
+                    else:
+
+                        response.category_event = ""
+                        await manager.broadcast(response.model_dump_json())
+
+                else:
+
+                    await manager.broadcast(response.model_dump_json())
+
+            except Exception as e:
+                logger.error(f"Error broadcasting event or saving event to DB: {e}")
 
 def categorize_event(dominant_frequency):
     if dominant_frequency < 0.5:
         return ""
     elif dominant_frequency < 3.0:
         return "earthquake"
-    elif dominant_frequency < 5.0:
+    elif dominant_frequency < 8.0:
         return "conventional_explosion"
     else:
         return "nuclear_like"